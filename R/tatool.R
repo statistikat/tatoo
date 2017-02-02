03fb22de287df21012ddb1da72b5c49ecf68aeb1
Stefan Fleck <stefan.fleck@statistik.gv.at>
2017-02-02 10:26:11 +0100

    reworked exports/imports

--- R/tatool.R
+++ R/tatool.R
@@ -3,11 +3,16 @@
 #' @section Foo functions:
 #' The foo functions ...
 #'
-#' @import assertthat openxlsx
-#' @import dtplyr
+#'
+#' @import hammr
+#' @export save_xlsx save_tex save_txt as_workbook write_worksheet
+#'
+#' @import assertthat openxlsx  dtplyr
 #' @importFrom magrittr %>%
 #' @importFrom data.table data.table as.data.table
 #' @importFrom foreach foreach %do%
+#'
+#'
 #' @docType package
 #' @name tatool
 NULL
