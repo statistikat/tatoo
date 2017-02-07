4fe55eb8506253ff632c046cb4c5dcb5b165a90b
Stefan Fleck <stefan.fleck@statistik.gv.at>
2017-02-07 15:55:51 +0100

    formatting

--- R/tatool.R
+++ R/tatool.R
@@ -7,10 +7,12 @@
 #' @import hammr
 #' @export save_xlsx save_tex save_txt as_workbook write_worksheet
 #'
+#'
 #' @import assertthat openxlsx  dtplyr
 #' @importFrom magrittr %>%
 #' @importFrom data.table data.table as.data.table
 #' @importFrom foreach foreach %do%
+#'
 #'
 #'
 #' @docType package
