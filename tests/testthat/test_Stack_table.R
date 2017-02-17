7e532b396e32208be0466aa424c1bbb60329b1b8
stefan <unabledispute@gmail.com>
2017-02-17 11:38:01 +0100

    inital test

--- tests/testthat/test_Stack_table.R
+++ tests/testthat/test_Stack_table.R
@@ -2,48 +2,62 @@ context("Stack_table")
 
 
 test_that("Stack_table works as expected", {
+  #* @testing stack_table
+
 
   # Generate test data
-  tmeta <- tt_meta(
-    table_id = 't001',
-    title = 'Table 1',
-    longtitle = 'Table of Numbers',
-    subtitle = 'A table that contains numbers but maybe also letters',
-    footer = 'a footer'
-  )
-
-  tdat <- data.frame(
-    x = letters[1:5],
-    y = letters[10:14]
-  )
-
-  expect_silent(tpub <- meta_table(tdat, tmeta))
-
-
-  tdat1 <- data.frame(
-    numbers = c(1.434, 190.3, 228.311, 5.210, 4321543),
-    animals = c('dog', 'cat', 'camel', 'pig', 'mouse'),
-    stringsAsFactors = FALSE
-  )
-
-  tdat2 <- data.frame(
-    numbers = c(1, 290, 0.311, 0.210, 1000),
-    animals = c('god', 'tac', 'lemac', 'gip', 'esuom'),
-    stringsAsFactors = FALSE
-  )
-
-
-  expect_silent(tmash <- mash_table(tdat1, tdat2))
-
-
-  tdat <- list()
-  for(i in seq_len(3)){
-    tdat[[i]] <- data.frame(
-      id = 1:6,
-      small = letters[i:(i+5)],
-      tall = LETTERS[i:(i+5)]
+    tm <- tt_meta(
+      table_id = 't001',
+      title = 'Table 1',
+      longtitle = 'Table of Numbers',
+      subtitle = 'A table that contains numbers but maybe also letters',
+      footer = 'a footer'
     )
-  }
+    tdat <- data.frame(
+      x = letters[1:5],
+      y = letters[10:14]
+    )
+    expect_silent(tmeta <- meta_table(tdat, meta = tm))
+
+
+    tdat1 <- data.frame(
+      numbers = c(1.434, 190.3, 228.311, 5.210, 4321543),
+      animals = c('dog', 'cat', 'camel', 'pig', 'mouse'),
+      stringsAsFactors = FALSE
+    )
+    tdat2 <- data.frame(
+      numbers = c(1, 290, 0.311, 0.210, 1000),
+      animals = c('god', 'tac', 'lemac', 'gip', 'esuom'),
+      stringsAsFactors = FALSE
+    )
+    expect_silent(tmash <- mash_table(
+      tdat1, tdat2, meta = tt_meta('t02', 'a mash table'))
+    )
+
+
+    tdat <- list()
+    for(i in seq_len(3)){
+      tdat[[i]] <- data.frame(
+        id = 1:6,
+        small = letters[i:(i+5)],
+        tall = LETTERS[i:(i+5)]
+      )
+    }
+    tcomp <- comp_table_list(
+      tdat,
+      titles = c('tab1', 'tab2', 'tab3'),
+      meta = tt_meta('t03', 'a comp table')
+    )
+
+
+  # Constructor works
+    tdat <- stack_table(tmeta, tmash, tcomp, meta = tt_meta(
+      table_id = 'rp1',
+      title = 'stack table 1',
+      longtitle = 'stack table 1 is a stack of tables',
+      subtitle = 'with a subtitle',
+      footer = 'that has a footer also'
+    ))
 
 
 
