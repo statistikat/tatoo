0b2170dcbc48502729e95cb8c1dbb3970b6671f7
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-03-25 16:19:07 +0100

    added examples

--- R/Composite_table.R
+++ R/Composite_table.R
@@ -17,6 +17,26 @@
 #' @aliases comp_table composite_table
 #' @family Tatto tables
 #' @export
+#'
+#' @examples
+#'
+#' comp_table(mean = df_mean, sd = df_sd)
+#'
+#' # ...........mean............     ............sd.............
+#' # 1    Species   length   width        Species   length   width
+#' # 2     setosa     5.01    3.43         setosa     0.35    0.38
+#' # 3 versicolor     5.94    2.77     versicolor     0.52    0.31
+#' # 4  virginica     6.59    2.97      virginica     0.64    0.32
+#'
+#'
+#' comp_table(mean = df_mean, sd = df_sd, id_vars = 'Species')
+#'
+#' # ..........     .....mean.....     ......sd......
+#' # 1    Species     length   width     length   width
+#' # 2     setosa       5.01    3.43       0.35    0.38
+#' # 3 versicolor       5.94    2.77       0.52    0.31
+#' # 4  virginica       6.59    2.97       0.64    0.32
+#'
 comp_table <- function(
   ...,
   id_vars = NULL,
