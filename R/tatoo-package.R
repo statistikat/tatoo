394a93328ad4e16c1e95eb1060152425ce3ea8ab
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-07-21 23:05:52 +0200

    Updated readme, vignette and package doc

--- R/tatoo-package.R
+++ R/tatoo-package.R
@@ -1,26 +1,20 @@
-#' Table tools: tools for combining data frames and exporting them to xlsx
+#' tatoo: Combine and Export Data Frames
 #'
-#' Tatoo ("Table Tools") provides three straight-forward helper to combine
-#' multiple `data.frames` to tables that are otherwise awkward to create in
-#' base-r, and a way to add metadata (id, title, ...) to a table. In addition,
-#' the Tatto_report class is provided as a convenient helper to write several
-#' Tatoo tables to a workbook, one table per worksheet.
+#' @section Functions:
 #'
-#' Tatoo tables and reports can directly be saved to .xlsx files, or convert to
-#' `Workbook` objects with `as_workbook()` so that you can process them further
-#' using the `openxlsx` package. While tatoo implements convenient print methods
-#' so that you can preview the tables you created in the console, most of the
-#' functionality provided by this package only makes real sense for .xlsx
-#' export.
-#'
-#' @section Functions: * [tag_table()]: add captioning (title, footer, ...) to a
-#'   table * [comp_table()]: like [cbind()] or [merge()], but retain
-#'   multi-column headings * [mash_table()]: combine data.frames so that their
-#'   rows or columns alternate * [stack_table()]: create a list of tables that
-#'   can be exported to xlsx, all tables on the same worksheet on top of each
-#'   others * [compile_report()]: create a list of tables that can be exported
+#' * [tag_table()]: add captioning (title, footer, ...) to a table
+#' * [comp_table()]: like [cbind()] or [merge()], but retain multi-column
+#'   headings
+#' * [mash_table()]: combine data.frames so that their
+#'   rows or columns alternate. Mash tables are stored as lists that can be
+#'   converted to data.tables, or you can use [rmash()] and [cmash()] to create
+#'   `data.frames` directly.
+#' * [stack_table()]: create a list of tables that can be exported to xlsx, all
+#'   tables on the same worksheet on top of each others
+#' * [compile_report()]: create a list of tables that can be exported
 #'   to xlsx, one table per worksheet (a Stacked_table also counts as one table)
-#'   * [as_workbook()] / [save_xlsx()]: export to excel
+#' * [as_workbook()] / [save_xlsx()]: To export any of the objects described
+#'   above to excel workbooks.
 #'
 #'
 #' @md
--- README.md
+++ README.md
@@ -1,19 +1,39 @@
 # tatoo
 
-Tatoo ("Table Tools") provides functions to combine data.frames in ways that 
-  require additional effort in  base R, and to add metadata (id, title, ...) 
-  that can be used for printing and xlsx export. The 'Tatoo_report' class is 
-  provided as a convenient helper to write several such tables to a workbook, 
-  one table per worksheet. In addition, the `Tatto_report`
-  class is provided as a convenient helper to write several such tables to a 
-  workbook, one table per worksheet.
+## Overview
+
+tatoo ("table tools") provides functions to combine data.frames in ways that 
+require additional effort in base R, and to add metadata (id, title, ...) 
+that can be used for printing and xlsx export. The 'Tatoo_report' class is 
+provided as a convenient helper to write several such tables to a workbook, 
+one table per worksheet. In addition, the `Tatto_report`
+class is provided as a convenient helper to write several such tables to a 
+workbook, one table per worksheet.
+  
+  
+## Installation
+
+``` r
+# tatoo is available from CRAN
+install.packages("tatoo")
+
+# Or you can install the development version from GitHub:
+install.packages("devtools")
+devtools::install_github("statistikat/tatoo")
+```
+
   
 ## Example
 
 ```R
-tag_table(head(cars), tt_meta(title = "Data about cars"))
-mash_table(head = head(cars), tail = tail(cars))
+tag_table(head(cars), tt_meta(table_id = "t1", title = "Data about cars"))
+mash_table(head = head(cars), tail = tail(cars), mash_method = "row")
 mash_table(head = head(cars), tail = tail(cars), mash_method = "col")
 comp_table(head = head(cars), tail = tail(cars))
 stack_table(head = head(cars), tail = tail(cars))
 ```
+
+None of the examples are particularily hard to do in base R, but tatoo provides
+functions with a clean interface and nice print and export methods for the 
+created objects. Please refer to the package vignette (`vignette("tatoo")`)
+for more examples
