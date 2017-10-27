aefa698ea6aae5d31437a44ef63cc1ef33f212b1
Stefan Fleck <stefan.b.fleck@gmail.com>
2017-10-27 12:10:26 +0200

    use a plain latex template instead of an unnecessary markdown step. added paramters for paper size and orientation

--- R/print_tex.R
+++ R/print_tex.R
@@ -27,31 +27,79 @@ print_tex.Mashed_table <- function(
 }
 
 
+#' Save Table as PDF
+#'
+#' @param x
+#' @param outfile
+#' @param mash_method
+#' @param id_vars
+#' @param insert_blank_row
+#' @param overwrite
+#' @param template
+#' @param papersize `character` scalar. Passed on to the latex command
+#'   `\\geometry` from the 'geometry' package. Valid values are: `a0paper,
+#'   a1paper, a2paper, a3paper, a4paper, a5paper, a6paper, b0paper, b1paper,
+#'   b2paper, b3paper, b4paper, b5paper, b6paper, c0paper, c1paper, c2paper,
+#'   c3paper, c4paper, c5paper, c6paper, b0j, b1j, b2j, b3j, b4j, b5j, b6j,
+#'   ansiapaper, ansibpaper, ansicpaper, ansidpaper, ansiepaper, letterpaper,
+#'   executivepaper, legalpaper`
+#' @param orientation `character` scalar. Passed on to the latex command
+#'   `\\geometry` from the 'geometry' package. Valid values are:
+#'   `portrait`, `landscape`
+#' @param keep_source
+#'
+#' @return
+#' @export
+#'
+#' @examples
 save_pdf <- function(
+  x,
+  ...,
+  papersize = "a4paper",
+  orientation = "portrait",
+  keep_source = FALSE,
+  template = system.file("templates", "save_tex.Rmd", package = "tatoo")
+){
+  UseMethod("save_pdf")
+}
+
+
+
+save_pdf.Mashed_table <- function(
   x,
   outfile,
   mash_method = attr(x, 'mash_method'),
   id_vars  = attr(x, 'id_vars'),
   insert_blank_row = attr(x, 'insert_blank_row'),
   overwrite = FALSE,
-  template = system.file("templates", "save_tex.Rmd", package = "tatoo"),
-  keep_source = FALSE
+  papersize = "a4paper",
+  orientation = "portrait",
+  keep_source = FALSE,
+  template = system.file("templates", "save_tex.Rmd", package = "tatoo")
 ){
+  assert_that(rlang::is_scalar_character(papersize))
+
   temp <- readLines(template)
+
   tex <- print_tex(x, insert_blank_row = insert_blank_row)
-  rmd <- paste(gsub("{TABLE}", tex, temp, fixed = TRUE), collapse = "\n")
-  tf <- tempfile()
-  writeLines(rmd, tf)
+  tex <- gsub("{TABLE}", tex, temp, fixed = TRUE)
+  tex <- gsub("{PAPERSIZE}", papersize, tex, fixed = TRUE)
+  tex <- gsub("{ORIENTATION}", orientation, tex, fixed = TRUE)
+  tex <- paste(tex, collapse = "\n")
+
+  tf <- paste0(tempfile(), c(".tex", ".pdf"))
+  writeLines(tex, tf[[1]])
+
+  withr::with_dir(
+    dirname(tf[[1]]),
+    tools::texi2pdf(tf[[1]])
+  )
+
+  file.copy(from = tf[[2]], to = outfile, overwrite = overwrite)
 
   if (keep_source){
-    of_source <- paste0(of, ".Rmd")
-    file.copy(tf, of)
+    file.copy(tf[[1]], paste0(outfile, ".tex"), overwrite = overwrite)
   }
 
-  rmarkdown::render(
-    tf,
-    output_format = "pdf_document",
-    output_file = outfile
-  )
 }
 
